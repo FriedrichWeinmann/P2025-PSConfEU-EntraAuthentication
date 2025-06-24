6d89d3145c433c19bd433f415bc0f592eae2c803
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2025-06-24 17:02:50 +0200

    updates

---
+++ code/01-GettingStarted.ps1
@@ -0,0 +1,66 @@
+﻿# failsafe
+return
+
+# Module: EntraAuth
+# https://github.com/FriedrichWeinmann/EntraAuth
+
+$clientID = '0dfa9522-80cc-4f67-943e-18418325068a'
+$tenantID = 'a948c2b3-8eb2-498a-9108-c32aeeaa0f90' # 'organizations'
+
+<#
+Create Application:
+- Graph: User.ReadBasic.All
+- Sharepoint: AllSites.Read, Sites.Search.All
+#>
+
+$resource = 'https://graph.microsoft.com/'
+$token = Connect-EntraService -ClientID $clientID -TenantID $tenantID -Resource $resource
+
+$token
+$token.AccessToken
+$token.AccessToken | Set-Clipboard
+
+# jwt.ms
+
+Invoke-RestMethod -Uri 'https://graph.microsoft.com/v1.0/me' -Headers @{
+	Authorization = "Bearer $($token.AccessToken)"
+}
+
+#-> Don't do manual labor! Get that header directly
+$token.GetHeader()
+
+
+# Moving on to Sharepoint
+#--------------------------
+$wayTooLongSharepointAPICall = "https://psfred.sharepoint.com/_api/search/query?querytext='contentclass:STS_Site'&selectproperties='Title,Path'&rowlimit=1"
+Invoke-RestMethod -Uri $wayTooLongSharepointAPICall -Headers $token.GetHeader()
+
+#-> Who's the better Audience?
+$token.Audience
+$token.Scopes
+
+$resourceSP = 'https://microsoft.sharepoint.com' # What Resource?
+$tokenSP = Connect-EntraService -ClientID $clientID -TenantID $tenantID -Resource $resourceSP
+$tokenSP.Audience
+
+#-> Getting the token is easy, not every API is
+$site = Invoke-RestMethod -Uri $wayTooLongSharepointAPICall -Headers $tokenSP.GetHeader()
+$site
+$site | ConvertTo-Json -Depth 10
+
+
+$site.query.PrimaryQueryResult.RelevantResults.Table.Rows.element.Cells.element
+
+# Refresh Tokens
+#-----------------
+
+# Story Time
+
+$token.RefreshToken
+
+$tokenSP2 = Connect-EntraService -RefreshTokenObject $token -Resource $resourceSP
+$tokenSP2
+$site = Invoke-RestMethod -Uri $wayTooLongSharepointAPICall -Headers $tokenSP2.GetHeader()
+$site.query.PrimaryQueryResult.RelevantResults.Table.Rows.element.Cells.element
+
+#-> Back to the slides
---
+++ code/02-AuthenticationFlows.ps1
@@ -0,0 +1,59 @@
+﻿# failsafe
+return
+
+<#
+Application Scopes
+vs.
+Delegate Scopes
+#>
+
+## Application Flows
+#-> Client Secret
+$secret = Get-Clipboard | ConvertTo-SecureString -AsPLaintext -Force
+Connect-EntraService -ClientID $clientID -TenantID $tenantID -ClientSecret $secret
+Invoke-EntraRequest users
+
+
+#-> Client Certificate
+$cert = New-SelfSignedCertificate -Subject "CN=PSCOnfEU" -CertStoreLocation Cert:\CurrentUser\My
+$bytes = $cert.GetRawCertData()
+[System.IO.File]::WriteAllBytes("C:\Temp\demo\cert.cer", $bytes)
+Connect-EntraService -ClientID $clientID -TenantID $tenantID -Certificate $cert
+Invoke-EntraRequest users
+
+#-> Directly from cert store (could be the LocalMachine store)
+Connect-EntraService -ClientID $clientID -TenantID $tenantID -CertificateName 'CN=PSCOnfEU'
+Invoke-EntraRequest users
+
+
+## Delegate Flows
+#-> Authorization Code
+
+#-> DeviceCode
+
+#-> Web Authentication Manager (WAM)
+Connect-AzAccount
+
+
+## Managed Identity
+
+$url = Get-Clipboard
+Invoke-RestMethod $url
+
+# Module: EntraAuth.Graph.Application
+Connect-EntraService -ClientID graph
+Add-EAGMsiScope -DisplayName psconfeudemo -Scope 'Users.Read.All' -Resource 'https://graph.microsoft.com/'
+
+
+<#
+Links:
+https://github.com/FriedrichWeinmann/EntraAuth
+https://github.com/FriedrichWeinmann/EntraAuth.Graph.Application
+https://jwt.ms
+
+Docs:
+https://github.com/FriedrichWeinmann/EntraAuth/blob/master/docs/overview.md
+
+Social Media:
+Bluesky: psfred.bsky.social
+#>
